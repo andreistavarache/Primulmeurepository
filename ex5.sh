e5727c0aac49a1bd7606f2d528c8932a13fb99e5
Eu <stavaracheandrei92@gmail.com>
2021-07-08 10:36:24 +0000

    nou fisier

--- ex5.sh
+++ ex5.sh
@@ -1,7 +1,27 @@
-read -p "Scrieti numarul de telefon dorit: " ip
+#!/bin/bash 
 
-if [[ $ip == ^[0-9]{3}-[0-9]{3}-[0-9]{3}$ ]]; then
-    echo Numar de telefon valid;
-else
-    echo "Numar de telefon invalid"
-fi
+  
+
+echo "Acesta este un tool de replicare a functiei WC." 
+
+read -p "Va rog, introduceti pathul catre fisierul dorit: " path 
+
+if [$path == ""];then 
+
+    echo -e "Nu ati introdus nimic. \nVa rog incercati!" 
+
+else 
+
+    #echo -e 'Pentru numararea caracterelor tastati 1. \nPentru numararea cuvintelor tastati 2. \nPentru numararea rand$    echo "Pentru numararea caracterelor tastati 1." ; echo "Pentru numararea cuvintelor tastati 2." ; echo "Pentru numa$fi 
+
+read actiunea 
+
+if [ $actiunea = "3" ];then 
+
+     echo "Numarul de randuri este: "  && awk 'END{print NR}' $path 
+
+else 
+
+    echo "Fisierul nu exista. Reincercati!" 
+
+fi 
